c75b182440d66d12601be5692f826e53f03afdc9
xuchenglong <xuchenglong@broadtext.com.cn>
2019-06-13 15:21:14 +0800

    修复三个接口的bug

--- ycadp-data-ac-api/src/main/java/com/broadtext/ycadp/data/ac/api/entity/TBDatasourceConfig.java
+++ ycadp-data-ac-api/src/main/java/com/broadtext/ycadp/data/ac/api/entity/TBDatasourceConfig.java
@@ -18,30 +18,43 @@ import java.util.Date;
 @EqualsAndHashCode(callSuper = true)
 public class TBDatasourceConfig extends AbstractBaseEntity{
     /** 数据源名称 */
+    @Column(name = "DATASOURCE_NAME")
     private String datasourceName;
     /** 数据源类型 */
+    @Column(name = "DATASOURCE_TYPE")
     private String datasourceType;
     /** 数据库名(MYSQL)/模式名(ORACLE)/架构名(DB2) */
+    @Column(name = "SCHEMA_DESC")
     private String schemaDesc;
     /** 数据源用户名 */
+    @Column(name = "DATASOURCE_USER_NAME")
     private String datasourceUserName;
     /** 数据源密码 */
+    @Column(name = "DATASOURCE_PASSWD")
     private String datasourcePasswd;
     /** 数据库驱动类 */
+    @Column(name = "DATASOURCE_DRIVER_CLASS")
     private String datasourceDriverClass;
     /** 字典SQL */
+    @Column(name = "DICT_SQL")
     private String dictSql;
     /** 备注 */
+    @Column(name = "REMARK")
     private String remark;
     /** 连接ip地址 */
+    @Column(name = "CONNECTION_IP")
     private String connectionIp;
     /** 连接端口号 */
+    @Column(name = "CONNECTION_PORT")
     private String connectionPort;
     /** 表数量 */
+    @Column(name = "TABLE_COUNT")
     private String tableCount;
     /** 文件ID */
+    @Column(name = "FILE_ID")
     private String fileId;
     /** DB2模式 */
+    @Column(name = "DB2_SCHEMA")
     private String db2Schema;
 
 }

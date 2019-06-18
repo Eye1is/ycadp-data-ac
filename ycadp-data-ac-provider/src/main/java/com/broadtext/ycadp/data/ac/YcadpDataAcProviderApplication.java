195e90d04e6aa5656934fed1d63edffd04a6fb70
xuchenglong <xuchenglong@broadtext.com.cn>
2019-06-18 09:50:47 +0800

    修改启动类

--- ycadp-data-ac-provider/src/main/java/com/broadtext/ycadp/data/ac/YcadpDataAcProviderApplication.java
+++ ycadp-data-ac-provider/src/main/java/com/broadtext/ycadp/data/ac/YcadpDataAcProviderApplication.java
@@ -2,12 +2,30 @@ package com.broadtext.ycadp.data.ac;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.kafka.annotation.EnableKafka;
 
+/**
+ * 启动类
+ *
+ * @author xuchenglong
+ */
+@EnableCircuitBreaker
+@EnableFeignClients
+@EnableHystrix
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableKafka
 public class YcadpDataAcProviderApplication {
 
+	/**
+	 *
+	 * 启动方法
+	 * @param args 启动参数
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(YcadpDataAcProviderApplication.class, args);
 	}

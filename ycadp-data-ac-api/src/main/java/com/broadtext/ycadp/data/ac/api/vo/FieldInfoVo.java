f20ddc21bff6f0ec15bfb4a60e2b31dde92329c4
ouhaoliang <ouhaoliang@broadtext.com.cn>
2019-09-03 17:27:27 +0800

    修改字典表传参

--- ycadp-data-ac-api/src/main/java/com/broadtext/ycadp/data/ac/api/vo/FieldInfoVo.java
+++ ycadp-data-ac-api/src/main/java/com/broadtext/ycadp/data/ac/api/vo/FieldInfoVo.java
@@ -21,6 +21,12 @@ public class FieldInfoVo {
     private java.lang.String dictId;
 
     private java.lang.String dictKey;
+
+    private java.lang.String dictType;
+
+    private java.lang.String treeId;
+
+    private java.lang.String treeType;
 }
 
 

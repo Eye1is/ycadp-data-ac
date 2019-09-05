deba92f3ae7ccf3253740bdd05e64d223aac7975
ouhaoliang <ouhaoliang@broadtext.com.cn>
2019-09-05 19:57:53 +0800

    countVo提交

---
+++ ycadp-data-ac-api/src/main/java/com/broadtext/ycadp/data/ac/api/vo/CountVo.java
@@ -0,0 +1,16 @@
+/*
+ * CountVo.java
+ * Created at 2019/9/5
+ * Created by ouhaoliang
+ * Copyright (C) 2019 Broadtext, All rights reserved
+ */
+
+package com.broadtext.ycadp.data.ac.api.vo;
+
+import lombok.Data;
+
+@Data
+public class CountVo {
+    private String datasourceId;
+    private String countSql;
+}

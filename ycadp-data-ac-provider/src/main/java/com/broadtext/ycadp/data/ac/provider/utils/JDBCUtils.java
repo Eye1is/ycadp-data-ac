c97bde180d251c42dc8b5c50a2d0bc50f7ee5644
ouhaoliang <ouhaoliang@broadtext.com.cn>
2020-05-12 17:23:03 +0800

    提交测试性能代码,测试后删掉

--- ycadp-data-ac-provider/src/main/java/com/broadtext/ycadp/data/ac/provider/utils/JDBCUtils.java
+++ ycadp-data-ac-provider/src/main/java/com/broadtext/ycadp/data/ac/provider/utils/JDBCUtils.java
@@ -10,6 +10,7 @@ package com.broadtext.ycadp.data.ac.provider.utils;
 import com.alibaba.druid.pool.DruidDataSource;
 import com.broadtext.ycadp.data.ac.api.constants.DataSourceType;
 import com.broadtext.ycadp.data.ac.api.entity.TBDatasourceConfig;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -54,92 +55,92 @@ public class JDBCUtils {
 //        String decrypt = AesUtil.decrypt(tbDatasourceConfig.getDatasourcePasswd(), "broadtext");
         switch(tbDatasourceConfig.getDatasourceType()){
             case DataSourceType.MYSQL:
-                datasourceInner.setUrl("jdbc:mysql://" + tbDatasourceConfig.getConnectionIp()
+                datasourceInner.setJdbcUrl("jdbc:mysql://" + tbDatasourceConfig.getConnectionIp()
                         + ":" + tbDatasourceConfig.getConnectionPort() + "/"
                         + tbDatasourceConfig.getSchemaDesc()
                         + "?useUnicode=true&characterEncoding=utf-8&useSSL=true&serverTimezone=UTC");//url
                 //验证连接有效与否的SQL,不同的数据配置不同
                 //检验连接是否有效的查询语句。如果数据库Driver支持ping()方法,
                 //则优先使用ping()方法进行检查,否则使用validationQuery查询进行检查。(Oracle jdbc Driver目前不支持ping方法)
-                datasourceInner.setValidationQuery("select 1");
+//                datasourceInner.setValidationQuery("select 1");
                 datasourceInner.setPassword(decrypt);//密码
                 break;
             case DataSourceType.ORACLE:
-                datasourceInner.setUrl("jdbc:oracle:thin:@" + tbDatasourceConfig.getConnectionIp()
+                datasourceInner.setJdbcUrl("jdbc:oracle:thin:@" + tbDatasourceConfig.getConnectionIp()
                         + ":" + tbDatasourceConfig.getConnectionPort() + "/"
                         + tbDatasourceConfig.getSchemaDesc());//url
                 //Oracle的验证语句
-                datasourceInner.setValidationQuery("select 1 FROM DUAL");
+//                datasourceInner.setValidationQuery("select 1 FROM DUAL");
                 datasourceInner.setPassword(decrypt);//密码
                 break;
             case DataSourceType.DB2:
-                datasourceInner.setUrl("jdbc:db2://" + tbDatasourceConfig.getConnectionIp()
+                datasourceInner.setJdbcUrl("jdbc:db2://" + tbDatasourceConfig.getConnectionIp()
                         + ":" + tbDatasourceConfig.getConnectionPort() + "/"
                         + tbDatasourceConfig.getSchemaDesc() + ":currentSchema=" + tbDatasourceConfig.getDb2Schema()+";");//url
-                datasourceInner.setDriverClassName("com.ibm.db2.jcc.DB2Driver");//db2要指定驱动名不然就默认COM.ibm.db2.jdbc.app.DB2Driver
+//                datasourceInner.setDriverClassName("com.ibm.db2.jcc.DB2Driver");//db2要指定驱动名不然就默认COM.ibm.db2.jdbc.app.DB2Driver
                 //DB2的验证语句
-                datasourceInner.setValidationQuery("select 1 from sysibm.sysdummy1;");
+//                datasourceInner.setValidationQuery("select 1 from sysibm.sysdummy1;");
                 datasourceInner.setPassword(decrypt);//密码
                 break;
             case DataSourceType.PostgreSQL:
-                datasourceInner.setUrl("jdbc:postgresql://" + tbDatasourceConfig.getConnectionIp()
+                datasourceInner.setJdbcUrl("jdbc:postgresql://" + tbDatasourceConfig.getConnectionIp()
                         + ":" + tbDatasourceConfig.getConnectionPort() + "/"
                         + tbDatasourceConfig.getSchemaDesc());//url
                 //PostgreSQL的验证语句
-                datasourceInner.setValidationQuery("select version();");
+//                datasourceInner.setValidationQuery("select version();");
                 datasourceInner.setPassword(decrypt);//密码
                 break;
             case DataSourceType.EXCEL:
-                datasourceInner.setUrl("jdbc:postgresql://" + tbDatasourceConfig.getConnectionIp()
+                datasourceInner.setJdbcUrl("jdbc:postgresql://" + tbDatasourceConfig.getConnectionIp()
                         + ":" + tbDatasourceConfig.getConnectionPort() + "/"
                         + tbDatasourceConfig.getSchemaDesc());//url
                 //PostgreSQL的验证语句
-                datasourceInner.setValidationQuery("select version();");
+//                datasourceInner.setValidationQuery("select version();");
                 datasourceInner.setPassword(tbDatasourceConfig.getDatasourcePasswd());//密码
                 break;
             default:
-                datasourceInner.setUrl(null);
+                datasourceInner.setJdbcUrl(null);
                 break;
         }
         //    private final String driverClass = "com.mysql.cj.jdbc.Driver"; 默认驱动url可辨别
-        datasourceInner.setUsername(tbDatasourceConfig.getDatasourceUserName());//用户名
+        datasourceInner.setUser(tbDatasourceConfig.getDatasourceUserName());//用户名
         //配置初始化大小、最小、最大
-        datasourceInner.setInitialSize(30);
-        datasourceInner.setMaxActive(maxActive);
-        datasourceInner.setMinIdle(0);
-        datasourceInner.setMaxWait(maxWait);
-        //设置从连接池获取连接时是否检查连接有效性,true时,每次都检查;false时,不检查
-        datasourceInner.setTestOnBorrow(false);
-        //设置从连接池获取连接时是否检查连接有效性,
-        //true时,如果连接空闲时间超过minEvictableIdleTimeMillis进行检查,否则不检查;false时,不检查
-        datasourceInner.setTestWhileIdle(true);
-        //设置往连接池归还连接时是否检查连接有效性,true时,每次都检查;false时,不检查
-        datasourceInner.setTestOnReturn(false);
-        datasourceInner.setPoolPreparedStatements(false);
-        //配置间隔多久启动一次DestroyThread,对连接池内的连接才进行一次检测,单位是毫秒
-        //检测时:1.如果连接空闲并且超过minIdle以外的连接,
-        //如果空闲时间超过minEvictableIdleTimeMillis设置的值则直接物理关闭。2.在minIdle以内的不处理
-        datasourceInner.setTimeBetweenEvictionRunsMillis(timeBtnEviRuns);
-        //配置一个连接在池中最小生存的时间,单位是毫秒
-        datasourceInner.setMinEvictableIdleTimeMillis(minEviIdleTime);
-        //连接泄露检查,打开removeAbandoned功能,
-        //连接从连接池借出后,长时间不归还,将触发强制回连接.
-        //回收周期随timeBetweenEvictionRunsMillis进行,如果连接为从连接池借出状态,并且未执行任何sql,
-        //并且从借出时间起已超过removeAbandonedTimeout时间,则强制归还连接到连接池中
-        datasourceInner.setRemoveAbandoned(true);
-        //超时时间,秒
-        datasourceInner.setRemoveAbandonedTimeout(rmAbdTimeout);
-        //关闭abandoned连接时输出错误日志,这样出现连接泄露时可以通过错误日志定位忘记关闭连接的位置
-        datasourceInner.setLogAbandoned(true);
-        //打开PSCache,并且指定每个连接上PSCache的大小
-        //只要maxPoolPreparedStatementPerConnectionSize>0,poolPreparedStatements就会被自动设定为true,参照druid的源码
-        datasourceInner.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPsPerConnSize);
+//        datasourceInner.setInitialSize(30);
+//        datasourceInner.setMaxActive(maxActive);
+//        datasourceInner.setMinIdle(0);
+//        datasourceInner.setMaxWait(maxWait);
+//        //设置从连接池获取连接时是否检查连接有效性,true时,每次都检查;false时,不检查
+//        datasourceInner.setTestOnBorrow(false);
+//        //设置从连接池获取连接时是否检查连接有效性,
+//        //true时,如果连接空闲时间超过minEvictableIdleTimeMillis进行检查,否则不检查;false时,不检查
+//        datasourceInner.setTestWhileIdle(true);
+//        //设置往连接池归还连接时是否检查连接有效性,true时,每次都检查;false时,不检查
+//        datasourceInner.setTestOnReturn(false);
+//        datasourceInner.setPoolPreparedStatements(false);
+//        //配置间隔多久启动一次DestroyThread,对连接池内的连接才进行一次检测,单位是毫秒
+//        //检测时:1.如果连接空闲并且超过minIdle以外的连接,
+//        //如果空闲时间超过minEvictableIdleTimeMillis设置的值则直接物理关闭。2.在minIdle以内的不处理
+//        datasourceInner.setTimeBetweenEvictionRunsMillis(timeBtnEviRuns);
+//        //配置一个连接在池中最小生存的时间,单位是毫秒
+//        datasourceInner.setMinEvictableIdleTimeMillis(minEviIdleTime);
+//        //连接泄露检查,打开removeAbandoned功能,
+//        //连接从连接池借出后,长时间不归还,将触发强制回连接.
+//        //回收周期随timeBetweenEvictionRunsMillis进行,如果连接为从连接池借出状态,并且未执行任何sql,
+//        //并且从借出时间起已超过removeAbandonedTimeout时间,则强制归还连接到连接池中
+//        datasourceInner.setRemoveAbandoned(true);
+//        //超时时间,秒
+//        datasourceInner.setRemoveAbandonedTimeout(rmAbdTimeout);
+//        //关闭abandoned连接时输出错误日志,这样出现连接泄露时可以通过错误日志定位忘记关闭连接的位置
+//        datasourceInner.setLogAbandoned(true);
+//        //打开PSCache,并且指定每个连接上PSCache的大小
+//        //只要maxPoolPreparedStatementPerConnectionSize>0,poolPreparedStatements就会被自动设定为true,参照druid的源码
+//        datasourceInner.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPsPerConnSize);
     }
 
     /**
      * 声明连接池
      */
-    private DruidDataSource datasourceInner  = new DruidDataSource();
+    private ComboPooledDataSource datasourceInner  = new ComboPooledDataSource();
     /**
      * 声明连接线程共享变量
      */
@@ -147,7 +148,7 @@ public class JDBCUtils {
     /**
      * 声明连接池线程共享变量
      */
-    private static ThreadLocal<DruidDataSource> dsContainer = new ThreadLocal<DruidDataSource>();
+    private static ThreadLocal<ComboPooledDataSource> dsContainer = new ThreadLocal<ComboPooledDataSource>();
 
     /**
      * 获取数据连接
@@ -214,7 +215,7 @@ public class JDBCUtils {
     /***关闭连接*/
     public void close() {
         try {
-            DruidDataSource druidDataSource = dsContainer.get();
+            ComboPooledDataSource druidDataSource = dsContainer.get();
             Connection conn = container.get();
             if (conn != null && datasourceInner!=null) {
                 conn.close();
